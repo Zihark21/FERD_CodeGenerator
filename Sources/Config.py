94c17c20df63c06bcaa50e0ae00df1675bc006d2
Zihark21 <37166910+Zihark21@users.noreply.github.com>
2024-12-02 09:05:39 -0600

    Update Character List
    Fixed character lists as there were some characters that were missing in each section.

--- Sources/Config.py
+++ Sources/Config.py
@@ -44,7 +44,7 @@ LISTS = load_json("Lists.json")
 CODE_DATABASE = load_json("Code_Database.json")
 
 # Define lists to be used in the GUI
-CHAR_LIST = sorted(set(CHAR["NTSC"].keys()).union(CHAR["PAL"].keys()))
+CHAR_LIST = CHAR["CHAR_LIST"]
 CLASS_LIST = list(CLASS["ID"])[1:]
 ITEM_LIST = list(ITEM["ID"])[1:]
 SECTION_HEADER = ("TkDefaultFont", 10, "bold")
