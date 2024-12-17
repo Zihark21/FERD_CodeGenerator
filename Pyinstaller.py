1a645529b15a887950e029f01a6a7fa9d185cac7
Zihark21 <37166910+Zihark21@users.noreply.github.com>
2024-12-17 11:43:13 -0600

    Code Fix
    All Skills code was incorrect. Added zip functionality to pyinstaller

--- Pyinstaller.py
+++ Pyinstaller.py
@@ -1,5 +1,5 @@
 import PyInstaller.__main__
-import os
+import os, zipfile
 
 def get_path(relative_path):
     return os.path.join(os.getcwd(), relative_path)
@@ -26,3 +26,8 @@ options = [
 ]
 
 PyInstaller.__main__.run(options)
+
+with zipfile.ZipFile("FE-RD-CC.zip", 'w') as zipf:
+    zipf.write('pyinstaller/dist/FE-RD-CC.exe', os.path.basename('FE-RD-CC.exe'))
+    zipf.write('LICENSE', os.path.basename('LICENSE'))
+    zipf.write('README.md', os.path.basename('README.md'))
