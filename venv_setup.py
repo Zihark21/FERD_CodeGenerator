6c2ec23526f759b99200558acae3a1b3cf0e95ac
Zihark <37166910+Zihark21@users.noreply.github.com>
2025-04-21 18:41:01 -0500

    Update venv_setup.py

--- venv_setup.py
+++ venv_setup.py
@@ -1,6 +1,7 @@
 import os, sys, subprocess, shutil
 
 if os.path.exists('.venv'):
+    print('Removing existing venv...')
     shutil.rmtree('.venv')
 
 files = ['RDCW.zip']
@@ -8,10 +9,12 @@ folders = ['src/__pycache__', 'ui/__pycache__', 'RDCW']
 
 for file in files:
     if os.path.exists(file):
+        print(f'Removing {file}...')
         os.remove(file)
 
 for folder in folders:
     if os.path.exists(folder):
+        print(f'Removing {folder}...')
         shutil.rmtree(folder)
 
 subprocess.run([sys.executable, '-m', 'venv', '.venv'])
